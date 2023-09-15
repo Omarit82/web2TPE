1aecd03370b34829a6bb84f424fb3048300ac67e
MatiiMolina <molinamatias63@gmail.com>
2023-09-15 10:24:37 -0300

    Agrego router y reglas htaccess. Y creo una tabla que contendra info de la db

--- index.php
+++ index.php
@@ -2,11 +2,30 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <base href="<?php echo BASE_URL?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>eComerce-Discos</title>
 </head>
 <body>
     <h1>eComerce-Discos</h1>
-
+    <table>
+        <tr>
+            <th>
+                Titulo
+            </th>
+            <th>
+                Autor/Cantante
+            </th>
+            <th>
+                Genero
+            </th>
+            <th>
+                Precio
+            </th>
+            <?php
+                
+            ?>
+        </tr>
+    </table>
 </body>
 </html>
