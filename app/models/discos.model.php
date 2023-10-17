7a76f2fff4fd22316d692cdf9f80b94cfb135788
Omarit82 <123607116+Omarit82@users.noreply.github.com>
2023-10-16 21:56:24 -0300

    mod discos.model

--- app/models/discos.model.php
+++ app/models/discos.model.php
@@ -10,26 +10,26 @@ class discosModel{
         $this->db = new PDO("mysql:host=".MYSQL_HOST.";dbname=".MYSQL_DB.";charset=utf8",MYSQL_USER,MYSQL_PASS);
     }
 
-    function getDiscos(){
+    public function getDiscos(){
         $query = $this->db->prepare('SELECT * FROM discos');
         $query->execute();
         $discos = $query->fetchAll(PDO::FETCH_OBJ);
         return $discos;
     }
-    function getDisco($id){
+    public function getDisco($id){
         $query = $this->db->prepare('SELECT * FROM discos WHERE id = ?');
         $query->execute([$id]);
         $discos = $query->fetch(PDO::FETCH_OBJ);
         return $discos;
     }
 
-    function insertDisco($nombre,$autor,$genero,$precio){
+    public function insertDisco($nombre,$autor,$genero,$precio){
         $query = $this->db->prepare('INSERT INTO discos(nombre,autor,genero,precio) VALUES (?,?,?,?)');
         $query->execute([$nombre,$autor,$genero,$precio]);
     
     }
 
-    function deleteDisco($id){
+    public function deleteDisco($id){
         
         $query = $this->db->prepare('DELETE FROM discos WHERE id = ?');
         $query->execute([$id]);
